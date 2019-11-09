9211a59db27f080bfa5e254b9cf937b67a1ea590
Simone Margaritelli <evilsocket@gmail.com>
2019-11-09 18:35:15 +0100

    misc: small fix or general refactoring i did not bother commenting

--- units_per_day_timeseries.go
+++ units_per_day_timeseries.go
@@ -9,20 +9,22 @@ import (
 
 func View(res *models.Results) models.Chart {
 	ts := chart.TimeSeries{
-		XValues: make([]time.Time, res.NumRows),
-		YValues: make([]float64, res.NumRows),
+		XValues: make([]time.Time, 0),
+		YValues: make([]float64, 0),
 	}
 
 	layout := "2006-01-02"
-	for i, row := range res.Rows {
-		day := row["day"].(string)
-		count := float64(row["registered"].(int64))
+	for _, row := range res.Rows {
+		if row["day"] != nil {
+			day := row["day"].(string)
+			count := float64(row["registered"].(int64))
 
-		if t, err := time.Parse(layout, day); err == nil {
-			ts.XValues[i] = t
-			ts.YValues[i] = count
-		} else {
-			log.Error("can't parse date '%s' as date layout %s", day, layout)
+			if t, err := time.Parse(layout, day); err == nil {
+				ts.XValues = append(ts.XValues, t)
+				ts.YValues = append(ts.YValues, count)
+			} else {
+				log.Error("can't parse date '%s' as date layout %s", day, layout)
+			}
 		}
 	}
 
